ae599767327d39e4eaa903354602913788ca7f47
Jxgill2121 <144986259+Jxgill2121@users.noreply.github.com>
2023-11-21 23:42:13 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-<script>
-    function toggleTheme() {
-        const body = document.body;
-        body.classList.toggle('light-theme');
-    }
-</script>
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('toggleTheme');
+
+    toggleButton.addEventListener('click', function() {
+        document.body.classList.toggle('light-theme');
+    });
+});
